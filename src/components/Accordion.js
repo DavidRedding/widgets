9dc78f008317c9110701070af6170c6765db9547
David Redding <redding.david35@gmail.com>
2021-12-26 11:43:42 -0600

    rendered item list & added styling to acc

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Accordion = (props) => (
-  <h2>We were passed {props.items.length} items </h2>
-);
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className="active title">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
 
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="accordion ui styled">{renderedItems}</div>;
+};
 export default Accordion;

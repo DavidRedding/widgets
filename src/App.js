97705d09873409e13b97622e0c0459191601ae2d
David Redding <redding.david35@gmail.com>
2021-12-26 10:40:53 -0600

    built items arr & passed to Acc as prop

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
-export default () => <h1>Widgets App</h1>;
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front-end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use react by creating components",
+  },
+];
+
+export default () => (
+  <div>
+    <h1>Widgets App</h1> <Accordion items={items} />
+  </div>
+);
---
+++ src/components/Accordion.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Accordion = () => <h2>Accordion Boys</h2>;
+
+export default Accordion;

ae8b1073db75efc757907391910d3bc42dba3e7c
David Redding <redding.david35@gmail.com>
2021-12-26 21:05:53 -0600

    map accepts i as 2nd param

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const renderedItems = items.map((item, i) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="active title">
+        <div
+          className="active title"
+          onClick={() => console.log(`Index: ${i} has been clicked`)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>

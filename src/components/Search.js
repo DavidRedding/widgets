47e39ee01e5dc266531f3d5fe51ac7abc125d180
David Redding <redding.david35@gmail.com>
2021-12-27 08:58:53 -0600

    initialize "term" state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <React.Fragment>
       <h4 style={{ marginLeft: "1rem" }}>Enter Search Term</h4>
       <div
         className="ui loading icon input"
-        style={{ width: "96%", marginLeft: "1rem" }}
+        style={{ width: "95%", marginLeft: "1rem" }}
       >
-        <input type="text" placeholder="Search..." />
+        <input
+          onChange={(e) => setTerm(e.target.value)}
+          value={term}
+          placeholder="Search..."
+        />
         <i className="user icon"></i>
       </div>
     </React.Fragment>

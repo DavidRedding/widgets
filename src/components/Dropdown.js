af83a9f9e5e1d94f88edb818494ea92977ee38a9
David Redding <redding.david35@gmail.com>
2021-12-28 12:35:35 -0600

    map function & display

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,9 +2,25 @@ import React from "react";
 // de-structure stops the need for props. prefix
 
 const Dropdown = ({ options }) => {
-  console.log(options[0]);
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        <span className="text">{option.label}</span>
+      </div>
+    );
+  });
 
-  return <h2>Dropdown Menu</h2>;
+  // Display
+  return (
+    <div className="ui form">
+      <label className="label">Select a Color</label>
+      <div className="ui fluid selection dropdown visible active ">
+        <div className="divider default text">Select Color</div>
+        <i className="dropdown icon"></i>
+        <div className="menu visible transition">{renderedOptions}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;

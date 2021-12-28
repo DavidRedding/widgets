0588e0848570d95042d261e58d95d96732d00e86
David Redding <redding.david35@gmail.com>
2021-12-28 10:02:15 -0600

    init Dropdown.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 // import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front-end javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use react by creating components",
-  },
-];
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 export default () => (
   <div className="ui container">
     <h1>Widgets App</h1>
-    <Search />
+    <Dropdown />
+    {/* <Search /> */}
     {/* <Accordion items={items} /> */}
   </div>
 );
+
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front-end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use react by creating components",
+//   },
+// ];
---
+++ src/components/Dropdown.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Dropdown = () => <h2>Drop it down</h2>;
+
+export default Dropdown;

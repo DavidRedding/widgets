4680565206fea13635561c9205321be4aa424898
David Redding <redding.david35@gmail.com>
2021-12-30 06:05:38 -0600

    initial searchbox styling

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -6,18 +6,27 @@ const options = [
   { label: "Arabic", value: "ar" },
   { label: "Hindi", value: "hi" },
 ];
-
+// prettier-ignore
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
 
   return (
-    <div>
-      <Dropdown
-        options={options}
-        onSelectedChange={setLanguage}
-        selected={language}
-        title={"Language"}
-      />
+    <div style={{ marginTop: "1rem" }}>
+
+      <label className="label">Enter Text:</label>
+      <div className="ui fluid focus input">
+        <input placeholder="Search a Phrase..." />
+      </div>
+
+      <div style={{ marginTop: "2rem" }}>
+        <Dropdown
+          options={options}
+          onSelectedChange={setLanguage}
+          selected={language}
+          title={"Language"}
+        />
+      </div>
+
     </div>
   );
 };

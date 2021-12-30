cfe0e7227019e08e1c0d310ebcd611c7dc6a0581
David Redding <redding.david35@gmail.com>
2021-12-29 19:53:50 -0600

    created language options array

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+const options = [
+  { label: "Afrikaans", val: "af" },
+  { label: "Arabic", val: "ar" },
+  { label: "Hindi", val: "hi" },
+];
 
 const Translate = () => {
   return (

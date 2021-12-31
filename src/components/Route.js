d0e75dc51eb8ac5812e9a6a4fbf76d0e9a645599
David Redding <redding.david35@gmail.com>
2021-12-31 08:30:06 -0600

    fixed err in test

--- src/components/Route.js
+++ src/components/Route.js
@@ -8,7 +8,11 @@ const Route = ({ path, children }) => {
     const onLocationChange = () => console.log(`Location Changed`);
 
     // Listening for that popState/navEvent
-    window.addEventListener('popstate', onLocationChange());
+    window.addEventListener('popstate', onLocationChange);
+    
+    // clean-up
+    return () => window.removeEventListener('popstate', onLocationChange); 
+
   }, []);
 
   // if the current url matches the comp's path, display the comp

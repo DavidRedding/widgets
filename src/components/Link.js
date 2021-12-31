3efd033918bd00dbd4ae214ebe4483c85e58f9be
David Redding <redding.david35@gmail.com>
2021-12-31 08:21:51 -0600

    Link.js event & Route.js listener test

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (e) => {
-    // stop page from relooading
+    // stop page from reloading
     e.preventDefault();
+
+    // changes URL w/o reload
+    window.history.pushState({}, "", href);
+
+    // tells Route comps that the URL has changed
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,17 @@
-/* if the current url matches the comp's path, display the comp */
+import { useEffect } from "react";
+// prettier-ignore
 
 const Route = ({ path, children }) => {
+  
+  useEffect(() => {
+    // callBack 
+    const onLocationChange = () => console.log(`Location Changed`);
+
+    // Listening for that popState/navEvent
+    window.addEventListener('popstate', onLocationChange());
+  }, []);
+
+  // if the current url matches the comp's path, display the comp
   return window.location.pathname === path ? children : null;
 };
 
